90b9f007902fcd05c9aa1760d178a7a9286aeb9c
Matthew Heffernan <mrhheffernan@gmail.com>
2026-02-16 10:29:14 -0800

    Finish implementing docs read-in from file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
-use std::fs;
+use rand::SeedableRng; // Provides a seedable rng
+use rand::prelude::SliceRandom; // Provides the shuffle trait
+use rand::rngs::StdRng;
+use std::fs; // Provides a base rng
 
 const FILEPATH: &str = "./input.txt";
+// A seed of your choice
+const SEED: u64 = 42;
 
 fn main() {
-    // Let there be an input dataset `docs`: list[str] of documents (e.g. a dataset of names)
-    let docs = fs::read_to_string(FILEPATH).unwrap();
+    // Let there be an RNG
+    let mut rng: StdRng = SeedableRng::seed_from_u64(SEED);
 
-    println!("{}", docs);
+    // Let there be an input dataset `docs`: list[str] of documents (e.g. a dataset of names)
+    let all_docs = fs::read_to_string(FILEPATH).unwrap();
+    let mut docs: Vec<&str> = all_docs.split('\n').collect();
+    docs.shuffle(&mut rng);
 }
